5eae56ef3ee677a57084532105587c8f36d3a0dc
hadi-awan <87779346+hadi-awan@users.noreply.github.com>
2025-01-07 16:51:34 -0500

    Initial commit

---
+++ src/main/java/com/github/hadi_awan/smarthome/smart_home_management/SmartHomeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.github.hadi_awan.smarthome.smart_home_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHomeManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/hadi_awan/smarthome/smart_home_management/SmartHomeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hadi_awan.smarthome.smart_home_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
